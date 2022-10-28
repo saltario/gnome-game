75599fb625802ea97f03ecf1faee170e578117b0
Vadim Shaulskiy <anarant.75@gmail.com>
2022-10-28 16:23:29 +0300

    Переработка стартового меню и меню новой игры (часть 1)

--- Gnome/Gnome/Colors.h
+++ Gnome/Gnome/Colors.h
@@ -1,43 +1,42 @@
 #pragma once
 
-namespace colors {
+#define COLORS_DEEP_BLUE	1
+#define COLORS_DEEP_YELLOW	6
 
-	////////////////// COLORS //////////////////
-	const int deepBlueTextColor = 1;
-	const int greenTextColor = 2;
-	const int redTextColor = 4;
-	const int purpleTextColor = 5;
-	const int deppYellowTextColor = 6;
-	const int whiteTextColor = 7;
-	const int greyTextColor = 8;
-	const int blueTextColor = 9;
-	const int lightGreenTextColor = 10;
-	const int cyanTextColor = 11;
-	const int lightRedTextColor = 12;
-	const int pinkTextColor = 13;
-	const int yellowTextColor = 14;
-	const int lightWhiteTextColor = 10;
+#define COLORS_LIGHT_GREEN	10
+#define COLORS_LIGHT_BLUE	11
+#define COLORS_LIGHT_RED	4
+
+#define COLORS_GREEN		2
+#define COLORS_RED			4
+#define COLORS_PURPLE		5
+#define COLORS_WHITE		7
+#define COLORS_GREY			8
+#define COLORS_BLUE			9
+#define COLORS_PINK			13
+#define COLORS_YELLOW		14
+
+namespace colors {
 
 	////////////////// RESERVED COLORS //////////////////
 
-	const int playerTextColor = yellowTextColor;
-	const int heroTextColor = yellowTextColor;
+	const int playerTextColor = COLORS_YELLOW;
+	const int heroTextColor = COLORS_YELLOW;
 
-	const int menuSeparatorColor = purpleTextColor;
-	const int menuColor = pinkTextColor;
+	const int menuSeparatorColor = COLORS_PURPLE;
+	const int menuColor = COLORS_PINK;
 
-	const int logoColor = redTextColor;
-	const int gameLoseColor = redTextColor;
-	const int gameWinColor = greenTextColor;
+	const int logoColor = COLORS_RED;
+	const int gameLoseColor = COLORS_RED;
+	const int gameWinColor = COLORS_GREEN;
 
-	const int helpColor = yellowTextColor;
+	const int helpColor = COLORS_YELLOW;
+	const int textColor = COLORS_YELLOW;
 
-	const int simpleHeroClass = lightGreenTextColor;
-	const int rareHeroClass = cyanTextColor;
-	const int legendaryHeroClass = pinkTextColor;
-	const int epicHeroClass = deppYellowTextColor;
+	const int simpleHeroClass = COLORS_LIGHT_GREEN;
+	const int rareHeroClass = COLORS_LIGHT_BLUE;
+	const int legendaryHeroClass = COLORS_PINK;
+	const int epicHeroClass = COLORS_DEEP_YELLOW;
 
 	////////////////// END RESERVED COLORS //////////////////
-
-	////////////////// END COLORS //////////////////
 }
